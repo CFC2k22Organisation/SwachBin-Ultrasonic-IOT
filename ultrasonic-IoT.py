f431eead80c229999bdfb631fae105e81b2024c8
Suneetha <33000263+Sunivihaan@users.noreply.github.com>
2022-10-30 19:44:53 +0530

    initial load

---
+++ ultrasonic-IoT.py
@@ -0,0 +1,49 @@
+#Code written for IBM Call for code 2K22 (SwachBin)
+import time
+import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
+
+ORG = "lw59h1"
+DEVICE_TYPE = "SwachBin"
+TOKEN = ")whpKZC4BqD8b2c&l2"
+DEVICE_ID = "e45f0142fbe8"
+server = ORG + ".messaging.internetofthings.ibmcloud.com";
+pubTopic1 = "iot-2/evt/Waste level/fmt/json";
+authMethod = "use-token-auth";
+token = TOKEN;
+clientId = "d:" + ORG + ":" + DEVICE_TYPE + ":" + DEVICE_ID;
+
+TRIG = 21
+ECHO = 20
+#GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+
+GPIO.output(TRIG, False)
+print("Calibrating.....")
+time.sleep(2)
+mqttc = mqtt.Client(client_id=clientId)
+mqttc.username_pw_set(authMethod, token)
+mqttc.connect(server, 1883, 60)
+
+while True:
+       GPIO.output(TRIG, True)
+       time.sleep(0.00001)
+       GPIO.output(TRIG, False)
+ 
+       while GPIO.input(ECHO)==0:
+          pulse_start = time.time()
+ 
+       while GPIO.input(ECHO)==1:
+          pulse_end = time.time()
+ 
+       pulse_duration = pulse_end - pulse_start
+ 
+       distance = pulse_duration * 17150
+       distance = round(distance+1.15, 2)
+       
+       mqttc.publish(pubTopic1, distance)
+       print("published")
+   
+       time.sleep(5)
